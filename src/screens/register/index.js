6ad0047338c5d065ae1f111303b1708078bc52b4
Lauro V. Figueirêdo <119360478+TheLitch@users.noreply.github.com>
2023-04-12 17:17:26 -0300

    Update index.js

--- src/screens/register/index.js
+++ src/screens/register/index.js
@@ -1,10 +1,73 @@
-import React, { useState } from 'react';
-import { View, } from 'react-native';
+import React,{ useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function RegisterScreen() {
   return (
-    <View>
-      
+    <View style={styles.container}>
+      <TextInput
+     style={styles.input}
+        placeholder="Nome"
+        onChangeText={()=> {}}
+        />
+         <TextInput
+     style={styles.input}
+        placeholder="Sobrenome"
+        onChangeText={()=> {}}
+        />
+      <TextInput
+       style={styles.input}
+        placeholder="Email"
+        onChangeText={()=> {}}
+      />
+     <TextInput
+     style={styles.input}
+        placeholder="Senha"
+        onChangeText={()=> {}}
+      /> 
+         <View style={styles.buttonContainer}>
+      <Button title="Criar" color="green" 
+      />
     </View>
+    <View style={styles.buttonContainer2}>
+      <Button title="Já tem uma conta? entre aqui" color="black"  
+      />
+    </View>
+
+    </View>
+
+ 
   );
 }
+const styles = StyleSheet.create({
+  buttonContainer: {
+    justifyContent: 'center',
+    alignItems: "center",
+    width: '90%',
+    backgroundColor:'green',
+    borderRadius: 7
+    
+  },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: "center",
+      backgroundColor: 'black',
+      width: '100%',
+      
+  },
+    input: {
+     backgroundColor:'white',
+     width: '90%',
+     marginBottom:15,
+     color: '#222',
+     fontSize: 18,
+     borderRadius: 7,
+     padding: 10
+    },
+    buttonContainer2: {
+      justifyContent: 'space-around',
+      alignItems: "center",
+      width: '90%',
+      
+    },
+});
